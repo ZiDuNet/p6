744d96d6da16fe060cf4f77b484e5284194d1c93
wushuo <email@example.com>
2020-10-16 16:19:50 +0800

    第2次提交

---
+++ src/main/java/Test/test2.java
@@ -0,0 +1,27 @@
+package Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class test2 {
+        public static void main(String[] args) throws IOException {
+           System.out.println("wushuo");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
